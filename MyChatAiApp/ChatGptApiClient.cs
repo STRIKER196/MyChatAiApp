d9ff5c7776e902ff2cdfe30587356c4a925852a0
Arek Stanik <arekstanik196@gmail.com>
2025-01-28 14:36:56 +0100

    Poczatek logiki 1 metoda

---
+++ MyChatAiApp/ChatGptApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChatAiApp
+{
+    internal class ChatGptApiClient
+    {
+        public async Task<string> GetChatResponseAsync(string userInput)
+        {
+            // W tym miejscu będziemy implementować komunikację z API
+            return string.Empty; // Na początek zwróćmy pusty ciąg znaków
+        }
+
+    }
+}
